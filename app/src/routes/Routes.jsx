1abd67ac631fda1eaea6a228439135384cacc534
caio <caioliberal@gmail.com>
2024-06-04 16:14:51 -0300

    CreateEvent

--- app/src/routes/Routes.jsx
+++ app/src/routes/Routes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 // import Register from '../pages/Register';
+import CreateEvent from '../pages/CreateEvent';
 
 function AppRoutes() {
   return (
@@ -11,6 +12,7 @@ function AppRoutes() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         {/* <Route path="/register" element={<Register />} /> */}
+        <Route path="/create-event" element={<CreateEvent />} />
       </Routes>
     </Router>
   );
