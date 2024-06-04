9eb308fed71c4c88e4511110f2363ca3aba5446f
MMp <mateusmoreirammp052@gmail.com>
2024-06-04 15:16:17 -0300

    login

--- app/src/routes/Routes.jsx
+++ app/src/routes/Routes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
-// import Login from '../pages/Login';
+import Login from '../pages/Login';
 // import Register from '../pages/Register';
 
 function AppRoutes() {
@@ -9,8 +9,8 @@ function AppRoutes() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} /> */}
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="/register" element={<Register />} /> */}
       </Routes>
     </Router>
   );
