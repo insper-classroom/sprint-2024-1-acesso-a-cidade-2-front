d7576fc5376b9edf3aeff81275561d39da456bcc
MMp <mateusmoreirammp052@gmail.com>
2024-06-05 08:52:15 -0300

    page admin feia

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -46,7 +46,7 @@ function Header() {
             variant="h6"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="/"
             sx={{
               mr: 2,
               display: { xs: 'none', md: 'flex' },
@@ -64,7 +64,7 @@ function Header() {
             variant="h5"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="/"
             sx={{
               mr: 2,
               display: { xs: 'flex', md: 'none' },
